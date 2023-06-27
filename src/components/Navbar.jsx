f8aa8c1d1e310b6ea6fd93e681a567559df3ae04
Yadnyesh Mahadeshwar <yadnyesh.mahadeshwar@gmail.com>
2023-06-27 17:36:34 +0530

    Website logo and Name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 navbar">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "120px" }} />
+        <img src={Logo} alt="Logo Image" style={{ width: "45px" }} />
       </div>
       {/* Menu */}
       <ul className="hidden md:flex">
